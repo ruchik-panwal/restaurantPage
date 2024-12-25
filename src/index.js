faddf254f494a19a46166020ea2d531dd8736cb9
Ruchik <ruchik.panwal@gmail.com>
2024-12-25 17:58:52 +0530

    tested the addresbox function and it works

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
 import "./styles.css";
 import pageLoad from "./homepage.js";
 import { menuhead, foodList } from "./menu.js";
+import { contactAddress } from "./contactUs.js";
 
 
 console.log("hello there");
@@ -14,3 +15,4 @@ console.log("hello there");
 // pageLoad();
 // menuhead();
 // foodList();
+// contactAddress();
