a38259fa98bb834359977ce0bef1e00b309a1d8b
Ruchik <ruchik.panwal@gmail.com>
2024-12-30 23:04:09 +0530

    Completed ContactUs

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./headerFooter.css";
 import "./styles.css";
 import pageLoad from "./homepage.js";
 import { menuhead, foodList } from "./menu.js";
-import { contactAddress , contactForm, addressMap} from "./contactUs.js";
+import { contactAddress, addressMap ,contactForm} from "./contactUs.js";
 
 
 console.log("hello there");
@@ -13,8 +13,8 @@ console.log("hello there");
 
 
 // pageLoad();
-menuhead();
-foodList();
-// contactAddress();
-// contactForm();
-// addressMap();
+// menuhead();
+// foodList();
+contactAddress();
+addressMap();
+contactForm();
