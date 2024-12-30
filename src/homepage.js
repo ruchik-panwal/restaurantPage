fabbd41ed6d3a7bfec158beabba745bfb3159817
Ruchik <ruchik.panwal@gmail.com>
2024-12-30 23:28:45 +0530

    removed description

--- src/homepage.js
+++ src/homepage.js
@@ -1,11 +1,5 @@
 const pageLoad = () => {
-
     const content = document.querySelector("#content");
-    const description = document.createElement("div");
-    description.innerHTML = "Food that Makes you go Crasy";
-
-    
-    content.appendChild(description);
 };
 
 export default pageLoad;
